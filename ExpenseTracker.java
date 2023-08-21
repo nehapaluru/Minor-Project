af0b95ff6000e5952c1d738f010a626782eef69a
Neha Paluru <133939764+nehapaluru@users.noreply.github.com>
2023-08-21 18:38:40 +0530

    Add files via upload

---
+++ ExpenseTracker.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Expense {
+    private String description;
+    private double amount;
+
+    public Expense(String description, double amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return description + " - $" + amount;
+    }
+}
+
+public class ExpenseTracker {
+    private List<Expense> expenses;
+
+    public ExpenseTracker() {
+        expenses = new ArrayList<>();
+    }
+
+    public void addExpense(String description, double amount) {
+        expenses.add(new Expense(description, amount));
+    }
+
+    public void viewExpenses() {
+        System.out.println("Expense List:");
+        for (Expense expense : expenses) {
+            System.out.println(expense);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ExpenseTracker expenseTracker = new ExpenseTracker();
+
+        while (true) {
+            System.out.println("Expense Tracker Menu:");
+            System.out.println("1. Add Expense");
+            System.out.println("2. View Expenses");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter expense description: ");
+                    scanner.nextLine(); // Consume newline
+                    String description = scanner.nextLine();
+                    System.out.print("Enter expense amount: ");
+                    double amount = scanner.nextDouble();
+                    expenseTracker.addExpense(description, amount);
+                    System.out.println("Expense added!");
+                    break;
+                case 2:
+                    expenseTracker.viewExpenses();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
